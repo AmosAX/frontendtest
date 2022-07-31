3a09ba986bde22f00bc7db4a185c199343f60803
ErikBjorkmanHA <erik.bjorkman@ha.ax>
2022-07-31 22:19:02 +0300

    Added user input
    Inserting message now creates textarea with the input text

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function sendMessage(){
 
+    const para = document.createElement("textarea");
+    const message = document.getElementById("message").value;
+
+    const node = document.createTextNode(message);
+    const rowbreak = document.createElement("br");
+    
+    para.appendChild(node);
+
+    const element = document.getElementById("testdiv");
+    element.appendChild(rowbreak);
+    element.appendChild(para);
+    
+
+    
+
+}
