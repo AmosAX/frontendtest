08fc6656de3c5b741ecb0e9e5be642659dea13b1
AmosAX <77676366+AmosAX@users.noreply.github.com>
2022-08-11 20:28:23 +0200

    We can now get all of the messages from the API
    Time to sort, clean and send

--- src/index.js
+++ src/index.js
@@ -1,28 +1,55 @@
-function sendMessage(){
+const APIToken =
+  "Bearer N31fRWVMZCtwU0JeZnBQdVBjTmlOImRzcTAxfl08cz1xR2lyWGFJfmo5JC5RNSc=";
+const APIUrl = "https://ha-slutuppgift-chat-do.westling.workers.dev";
+const APIMessages =
+  "https://ha-slutuppgift-chat-do.westling.workers.dev/api/messages";
 
-    const message = document.getElementById("message");
+function sendMessage() {
+  const message = document.getElementById("message");
 
-    if(message.value != ""){
+  if (message.value != "") {
+    const para = document.createElement("textarea");
+    para.readOnly = true;
 
-        const para = document.createElement("textarea");
-        para.readOnly = true;
-    
+    const node = document.createTextNode(message.value);
+    const rowbreak = document.createElement("br");
 
-        const node = document.createTextNode(message.value);
-        const rowbreak = document.createElement("br");
-        
-        para.appendChild(node);
-    
-        const element = document.getElementById("messagebox");
-        element.appendChild(rowbreak);
-        element.appendChild(para);
+    para.appendChild(node);
 
-        //scrolls away the oldest  messages
-        element.scrollTop = element.scrollHeight;
-    
-        message.value = "";
+    const element = document.getElementById("messagebox");
+    element.appendChild(rowbreak);
+    element.appendChild(para);
+
+    //scrolls away the oldest  messages
+    element.scrollTop = element.scrollHeight;
+
+    message.value = "";
+  }
+}
+/*
+function getMessagesFromAPI() {
+  fetch(APIUrl)
+    .then((response) => {
+      console.log("This is working"); // handle the response
+    })
+    .catch((error) => {
+      console.log("This is NOT working");
+      // handle the error
+    });
+}
+*/
+
+async function getMessagesFromAPI() {
+  let x = fetch(
+    "https://ha-slutuppgift-chat-do.westling.workers.dev/api/messages",
+    {
+      method: "get",
+      headers: {
+        Authorization:
+          "Bearer N31fRWVMZCtwU0JeZnBQdVBjTmlOImRzcTAxfl08cz1xR2lyWGFJfmo5JC5RNSc=",
+      },
     }
-
-
-
+  )
+    .then((x) => x.text())
+    .then((y) => (document.getElementById("chatlog").innerHTML = y));
 }
